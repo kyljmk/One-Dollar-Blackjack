e5740efd0f9c75fdfa3bf770e5b564ba254cd2ce
kyljmk <k.mansonkullin@googlemail.com>
2023-01-05 02:40:21 +0100

    Add Initial Cards Score Calculation

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ let dealerFirst = document.getElementById("dealer-first");
 let dealerSecond = document.getElementById("dealer-second");
 let playerFirst = document.getElementById("player-first");
 let playerSecond = document.getElementById("player-second");
+let dealerScore = document.getElementById("dealer-score");
+let playerScore = document.getElementById("player-score");
+let dealerCards;
+let playerCards;
+
+const cardConverter = (card) => {
+  if (card === "QUEEN" || card === "JACK" || card === "KING") {
+    return 10;
+  }
+  if (card === "ACE") {
+    return 11;
+  } else {
+    return Number(card);
+  }
+};
 
 const intialDeal = async () => {
   const res = await fetch(
@@ -11,9 +26,11 @@ const intialDeal = async () => {
   const data = await res.json();
   console.log(data);
   dealerFirst.src = data.cards[0].images.png;
-  dealerSecond.src = data.cards[1].images.png;
   playerFirst.src = data.cards[2].images.png;
   playerSecond.src = data.cards[3].images.png;
+  dealerScore.textContent += cardConverter(data.cards[0].value);
+  playerScore.textContent +=
+    cardConverter(data.cards[2].value) + cardConverter(data.cards[3].value);
 };
 
 const startGame = async () => {
